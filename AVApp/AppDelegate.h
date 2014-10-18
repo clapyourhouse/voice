878bb67e20ae10cb81f6af26c9067aabf5b2f288
北村 彰悟 <kitamura@infocity.co.jp>
2014-10-18 22:29:31 +0900

    Initial Commit

---
+++ AVApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVApp
+//
+//  Created by 北村 彰悟 on 2014/10/18.
+//  Copyright (c) 2014年 北村 彰悟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AVApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AVApp
+//
+//  Created by 北村 彰悟 on 2014/10/18.
+//  Copyright (c) 2014年 北村 彰悟. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
